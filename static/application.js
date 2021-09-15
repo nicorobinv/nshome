64718e398c7d8bcdc21f48eb59a94b67fc7ec0e6
nicorobinv <nicorobinv@gmail.com>
2021-09-15 22:32:20 +0900

    room notifications
    update

--- static/application.js
+++ static/application.js
@@ -8,11 +8,28 @@ room.hidden = true;
 
 let roomName;
 
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${input.value}`);
+  });
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -21,9 +38,7 @@ function test() {
 }
 
 function handleRoomSubmit(event) {
-  console.log(event);
   event.preventDefault();
-
   const input = form.querySelector("input");
   socket.emit("enter_room", input.value, showRoom);
   roomName = input.value;
@@ -32,3 +47,13 @@ function handleRoomSubmit(event) {
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("someone left!");
+});
+
+socket.on("new_message", addMessage);
